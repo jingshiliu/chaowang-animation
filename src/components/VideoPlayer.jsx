0e5f5f561f010dc21bba17e6f30bdbcd94aed398
Jingshi Liu <jingshiliu07@gmail.com>
2023-03-09 00:34:35 -0500

    removed onClick

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function VideoPlayer({autoplay, videoID, control, onClick}) {
+function VideoPlayer({autoplay, videoID}) {
+    console.log('VideoPlayer', videoID)
     return (
         <>
             <iframe className={'VideoPlayer'}
-                    onClick={onClick}
                     width="100%"
                     height="100%"
                     src={`https://www.youtube.com/embed/${videoID}?autoplay=${autoplay? 1 : 0}&mute=1`}
-                    title="YouTube video player" frameBorder="0"
+                    title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
                     allowFullScreen></iframe>
         </>
